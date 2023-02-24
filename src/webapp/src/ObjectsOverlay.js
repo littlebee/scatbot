1c2a3d750ef3c6549f2446d100ef1262f628459d
Bee <bee.wilkerson@ymail.com>
2023-02-24 07:52:28 -0800

    fix issue with overlay objects getting stuck on screen

--- src/webapp/src/ObjectsOverlay.js
+++ src/webapp/src/ObjectsOverlay.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { v4 as uuidv4 } from "uuid";
 
 import st from "./ObjectsOverlay.module.css";
 
@@ -19,8 +20,11 @@ export function ObjectsOverlay({ objects, filterConfidence }) {
       height: bottom - top,
       width: right - left,
     };
+    // Use a uuid key here because we don't have a good way of uniquely identifying
+    // these and having diff objects with the same key causes React to leave ghost
+    // recog objects that wont go away until you turn off the overlay
     elements.push(
-      <div className={st.objectSquare} style={style} key={left * top}>
+      <div className={st.objectSquare} style={style} key={uuidv4()}>
         <div className={st.objectClassification}>{obj.classification}</div>
         <div className={st.objectConfidence}>{obj.confidence.toFixed(3)}</div>
       </div>
